2680bc9325dab9f7cd4afe1a9784faf297e5ff6b
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-08-04 22:43:43 -0300

    chore: create project setup

---
+++ src/axios/index.js
@@ -0,0 +1,6 @@
+import Factory from './factory';
+
+const coreApiUrl = '';
+const commonInstance = Factory(coreApiUrl);
+
+export default commonInstance;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+
+import './styles/main.scss';
+
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+	render: h => h(App),
+}).$mount('#root');
